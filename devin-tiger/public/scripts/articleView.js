b18cab9d6c6ecfaaa3d021d113ce7f0c8261ee86
playing_tygre <kymiehsu123@gmail.com>
2018-01-06 16:46:32 -0800

    added readme.md

--- devin-tiger/public/scripts/articleView.js
+++ devin-tiger/public/scripts/articleView.js
@@ -115,7 +115,7 @@ var app = app || {};
 
     article.insertRecord();
 
-    // REVIEW: The following line of code redirects the user back to the home page after submitting the form.
+    // REVIEWED: The following line of code redirects the user back to the home page after submitting the form.
     window.location = '../';
   }
 
@@ -131,11 +131,11 @@ var app = app || {};
   };
 
   articleView.initAdminPage = () => {
-    // REVIEW: We use .forEach() here because we are relying on the side-effects of the callback function: appending to the DOM. The callback is not required to return anything.
+    // REVIEWED: We use .forEach() here because we are relying on the side-effects of the callback function: appending to the DOM. The callback is not required to return anything.
     var template = Handlebars.compile($('#author-stats').text());
     app.Article.numWordsByAuthor().forEach(stat => $('.author-stats').append(template(stat)));
 
-    // REVIEW: Simply write the correct values to the page:
+    // REVIEWED: Simply write the correct values to the page:
     $('#blog-stats .articles').text(app.Article.all.length);
     $('#blog-stats .words').text(app.Article.numWordsAll());
   };
